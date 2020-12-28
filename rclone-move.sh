449b1108c17023d3df07c9299902a096d107df01
NativeIT <68449281+nativeit@users.noreply.github.com>
2020-12-27 20:49:41 -0500

    Create rclone-move.sh

---
+++ rclone-move.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Last revised 2020-12-27
+# Usage - $ sh rclone-move.sh -b [specify bandwidth limit as an integer MB/s]
+# This script is designed to work in conjunction with Virtualmin's scheduled backups stored locally in /usr/share/backup/servers. When executed, this will
+# take the files in the backup folder and move them to Backblaze B2 storage already configured.
+
+
+# RClone Config file
+RCLONE_CONFIG=/root/.config/rclone/rclone.conf
+SCREEN_NAME=$(basename "$0" | cut -d '.' -f 1)
+export RCLONE_CONFIG
+export SCREEN_NAME
+
+# Exit if script is already running
+if ! [[ $(screen -S "$SCREEN_NAME" -Q select .) ]]; then
+	echo "$SCREEN_NAME is running, exiting..."
+     exit 1
+fi
+
+usage()
+{
+    echo "usage: rclone-move.sh [-b | --bandwidth specify bandwidth as an integer | -h | --help shows this message]"
+}
+
+if [ "$1" != "" ]; then
+
+BANDWIDTH=
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -b | --bandwidth )	shift
+                                BANDWIDTH=$1
+				export BANDWIDTH
+                                ;;
+        -h | --help )           usage
+                                exit 0
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+screen -dmS $SCREEN_NAME bash -c 'rclone move --bwlimit "$BANDWIDTH"M --transfers 8 --checkers 10 --update --log-file /usr/share/backup/log/rclone-move_$NOW.log /usr/share/backup/servers backblaze:HOST-WEB01/$NOW 2>&1 | tee /usr/share/backup/log/rclone-move.log'
+else
+screen -dmS $SCREEN_NAME bash -c 'rclone move --bwlimit 4M --transfers 8 --checkers 10 --update --log-file /usr/share/backup/log/rclone-move_$NOW.log /usr/share/backup/servers backblaze:HOST-WEB01/$NOW 2>&1 | tee /usr/share/backup/log/rclone-move.log'
+fi
