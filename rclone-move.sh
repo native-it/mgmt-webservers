1c64acacbe1f038b94967921a375662a85673858
NativeIT <68449281+nativeit@users.noreply.github.com>
2020-12-27 20:53:38 -0500

    Update rclone-move.sh

--- rclone-move.sh
+++ rclone-move.sh
@@ -6,8 +6,9 @@
 # take the files in the backup folder and move them to Backblaze B2 storage already configured.
 
 
-# RClone Config file
+# Set variables
 RCLONE_CONFIG=/root/.config/rclone/rclone.conf
+NOW=$(date +"%Y-%m-%d")
 SCREEN_NAME=$(basename "$0" | cut -d '.' -f 1)
 export RCLONE_CONFIG
 export SCREEN_NAME
@@ -18,11 +19,13 @@ if ! [[ $(screen -S "$SCREEN_NAME" -Q select .) ]]; then
      exit 1
 fi
 
+# Show help message when using -h
 usage()
 {
     echo "usage: rclone-move.sh [-b | --bandwidth specify bandwidth as an integer | -h | --help shows this message]"
 }
 
+# Set bandwidth limit using -b
 if [ "$1" != "" ]; then
 
 BANDWIDTH=
@@ -42,6 +45,7 @@ while [ "$1" != "" ]; do
     shift
 done
 
+# Run rclone command using set bandwidth, otherwise default to 4M
 screen -dmS $SCREEN_NAME bash -c 'rclone move --bwlimit "$BANDWIDTH"M --transfers 8 --checkers 10 --update --log-file /usr/share/backup/log/rclone-move_$NOW.log /usr/share/backup/servers backblaze:HOST-WEB01/$NOW 2>&1 | tee /usr/share/backup/log/rclone-move.log'
 else
 screen -dmS $SCREEN_NAME bash -c 'rclone move --bwlimit 4M --transfers 8 --checkers 10 --update --log-file /usr/share/backup/log/rclone-move_$NOW.log /usr/share/backup/servers backblaze:HOST-WEB01/$NOW 2>&1 | tee /usr/share/backup/log/rclone-move.log'
